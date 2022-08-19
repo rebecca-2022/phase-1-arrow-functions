9c6beda4f50510301f90a5584430a800e2e5865a
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-19 16:35:30 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(number1 = 2000, number2 = 100) {
+    let result = number1 / number2;
+    return result;
+}
+console.log(divide());
+const square = number => {
+    return number * number;
+}
+const add = (number1, number2) => number1 + number2;
